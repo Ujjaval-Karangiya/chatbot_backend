e59ad42d6ad02f2794f4986f6632900c2fcfe274
Ujjaval-Karangiya <ujjavalk2401@gmail.com>
2026-07-03 16:25:01 +0530

    new project is done

--- src/app.js
+++ src/app.js
@@ -4,7 +4,22 @@ const postroute = require("./routes/post.route");
 const cookieParser = require("cookie-parser");
 
 const app = express();
-app.use(express.json());
+
+// Custom CORS Middleware to allow requests from frontend port 5173
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
+app.use(express.json({ limit: "15mb" }));
+app.use(express.urlencoded({ limit: "15mb", extended: true }));
 app.use(cookieParser());
 app.use("/api/auth", authroute);
 app.use("/api/posts", postroute);
